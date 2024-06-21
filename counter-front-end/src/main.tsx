6acc316cb558f41bcca7528be4a1ee0b97a01fdd
SergeiT <t9111018075@gmail.com>
2024-06-21 16:05:54 +0200

    result of 5.2

--- counter-front-end/src/main.tsx
+++ counter-front-end/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+// this manifest is used temporarily for development purposes
+const manifestUrl = 'https://github.com/sergey-bpm/FuncC_course_public/blob/main/manifest.json';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </React.StrictMode>,
+  </TonConnectUIProvider>,
 )
