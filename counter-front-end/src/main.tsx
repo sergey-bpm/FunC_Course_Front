2392907ad2b5c22f9112c36704d506cc654aada3
SergeiT <t9111018075@gmail.com>
2024-06-22 21:32:31 +0200

    workflow

--- counter-front-end/src/main.tsx
+++ counter-front-end/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://github.io/sergey-bpm/FunC_Course/counter-front-end/Icon4Git.PNG';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
