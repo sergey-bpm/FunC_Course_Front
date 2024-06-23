541efd7d7e01b820724409b884cb7835ab75fcd0
SergeiT <t9111018075@gmail.com>
2024-06-23 13:30:01 +0200

    2v

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: '/FunC-Course-Front/',
+  base: '/FunC_Course_Front/',
 });
