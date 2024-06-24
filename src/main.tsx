fcab75d4d70221f22280bb1054c5426839b0e4d9
SergeiT <t9111018075@gmail.com>
2024-06-24 16:34:27 +0200

    main

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,21 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // // this manifest is used temporarily for development purposes
-// const manifestUrl = 'https://sergey-bpm.github.io/FunC-Course-Front/tonconnect-manifest.json';
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <TonConnectUIProvider manifestUrl={manifestUrl}>
-//     <App />
-//   </TonConnectUIProvider>,
-// )
-
 const manifestUrl = 'https://raw.githubusercontent.com/sergey-bpm/FunC_Course_Front/78153ec20b668d621dea756f1a8046c490696912/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <>
-    <link rel="manifest" href="tonconnect-manifest.json" crossOrigin="use-credentials" />
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <App />
-    </TonConnectUIProvider>
-  </>,
-);
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+)
+
+// const manifestUrl = 'https://raw.githubusercontent.com/sergey-bpm/FunC_Course_Front/78153ec20b668d621dea756f1a8046c490696912/tonconnect-manifest.json';
+
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <>
+//     <link rel="manifest" href="tonconnect-manifest.json" crossOrigin="use-credentials" />
+//     <TonConnectUIProvider manifestUrl={manifestUrl}>
+//       <App />
+//     </TonConnectUIProvider>
+//   </>,
+// );
