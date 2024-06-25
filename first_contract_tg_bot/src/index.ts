9dfe9388ec16af34c6a0f82f8c569dd07269b516
SergeiT <t9111018075@gmail.com>
2024-06-25 14:24:39 +0200

    all 6.4

--- first_contract_tg_bot/src/index.ts
+++ first_contract_tg_bot/src/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import { Telegraf } from "telegraf";
 import { beginCell, toNano } from "ton-core";
 import qs from "qs";
+import { message } from "telegraf/filters";
 
 dotenv.config();
 
@@ -19,8 +20,9 @@ bot.start((ctx) =>
     })
   );
 
-  bot.hears ("Increment by 1", (ctx) => {
+bot.on(message("web_app_data"), (ctx) => ctx.reply("ok"));
 
+bot.hears ("Increment by 1", (ctx) => {
     const msg_body = beginCell()
       .storeUint(1, 32) // OP code
       .storeUint(1, 32) // increment_by value
@@ -44,14 +46,56 @@ bot.start((ctx) =>
   });
 });
 
+
   bot.hears("Deposit 0.6 TON", (ctx) => {
-    // TODO: send deposit transaction
-    ctx.reply("Deposited 0.6 TON");
+    const msg_body = beginCell().storeUint(2, 32).endCell();
+
+      
+    let link = `https://app.tonkeeper.com/transfer/${
+        process.env.SC_ADDRESS
+    }?${qs.stringify(
+        {
+            text: "Deposit 0.6 TON",
+            amount: toNano("0.6").toString(10),
+            bin: msg_body.toBoc({ idx: false}).toString("base64"),
+        }
+    )}`;
+
+    ctx.reply("To deposit 0.6 TON please sign a transaction:", {
+      reply_markup: {
+        inline_keyboard: [
+          [{
+              text: "Sign transaction",
+              url: link,
+            }]
+        ]
+      }
+    });
   });
-  
-  bot.hears ("Withdraw 0.2 TON", (ctx) => {
-    // TODO: send withdraw transaction
-    ctx.reply("Withdrew 0.2 TON");
+
+  bot.hears("Withdraw 0.2 TON", (ctx) => {
+    const msg_body = beginCell().storeUint(3, 32).storeCoins(toNano(`0.2`)).endCell();
+      
+    let link = `https://app.tonkeeper.com/transfer/${process.env.SC_ADDRESS}
+    ?${qs.stringify({
+            text: "Withdraw 0.2 TON",
+            amount: toNano("0.05").toString(10),
+            bin: msg_body.toBoc({ idx: false}).toString("base64"),
+            }
+    )}`;
+        
+    ctx.reply("To withwraw 0.2 TON please sign a transaction:", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Sign transaction",
+              url: link,
+            }
+          ]
+        ]
+      }
+    });
   });
 
   bot.launch();
